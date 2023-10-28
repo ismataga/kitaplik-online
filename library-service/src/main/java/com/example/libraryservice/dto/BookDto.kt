a9fce05d403609c2283ab9fd00da18bd9bb931fa
ismet <ismeta@goldenpay.az>
2023-10-28 06:44:10 +0400

    Add BookDto

--- library-service/src/main/java/com/example/libraryservice/dto/BookDto.kt
+++ library-service/src/main/java/com/example/libraryservice/dto/BookDto.kt
@@ -1,8 +1,11 @@
 package com.example.libraryservice.dto
 
-data class BookDto @JvmOverloads constructor(
-    val title: String? = "",
-    val bookYear: Int? = 0,
-    val author: String? = "",
-    val pressName: String? = ""
-)
+data class BookDto @JvmOverloads constructor(val id: BookIdDto? = null,
+                                             val title: String? = "",
+                                             val year: Int? = 0,
+                                             val author: String? = "",
+                                             val pressName: String? = "") {
+
+
+
+}
